ccb43b356aae33b9f6e865123aaa5290b2a6ccb7
Yashwant <yashswami284@gmail.com>
2022-10-06 03:35:19 +0530

    Video 32 completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     
     render() {
-        let {title, description, imageUrl, newsUrl } = this.props;
+        let {title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
                     <img src={!imageUrl?"https://www.ctvnews.ca/polopoly_fs/1.4798178.1580915917!/httpImage/image.png_gen/derivatives/landscape_300/image.png":imageUrl} className="card-img-top" alt="..."/>
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:1}}> {source} </span>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
+                            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date (date).toGMTString()}</small></p>
                             <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
